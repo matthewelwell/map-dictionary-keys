a65fba198282448847faa8b97d519b24f1c3fb44
Matthew Elwell <matthewe@solidstategroup.com>
2020-05-03 22:06:05 +0100

    fix tests

--- tests/test_map_dictionary_keys.py
+++ tests/test_map_dictionary_keys.py
@@ -2,10 +2,6 @@ from .test_data import input_dict, expected_output
 from map_dictionary_keys import map_dictionary_keys
 
 
-def to_upper(string):
-    return string.upper()
-
-
-class TestMapDictionaryKeys(object):
+class TestMapDictionaryKeys:
     def test_valid_case(self):
-        assert map_dictionary_keys.map_dictionary_keys(input_dict, to_upper) == expected_output
+        assert map_dictionary_keys(input_dict, lambda key: key.upper()) == expected_output
